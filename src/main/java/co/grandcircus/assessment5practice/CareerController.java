21d1db4fc6ac9694affe46961a520af0d1d34020
supriya <supriya7458@gmail.com>
2020-11-11 23:43:37 -0500

    first commit

---
+++ src/main/java/co/grandcircus/assessment5practice/CareerController.java
@@ -0,0 +1,55 @@
+package co.grandcircus.assessment5practice;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CareerController {
+	
+	@RequestMapping("/")
+	public String yearSelection()
+	{
+		return "year-selection";
+	}
+
+	@RequestMapping("/submit-details")
+	public String submitDetails(@RequestParam String name,@RequestParam int year,Model model)
+	{
+		
+		if(!Character.isUpperCase(name.charAt(0)))
+		{
+			model.addAttribute("message","your name does not follow guidelines");
+			return "sorry";
+		}
+		else if(year<1943)
+				{
+				model.addAttribute("message","birth year is out of range");
+				return "sorry";
+				}
+		else if(year>2003)
+				{
+				model.addAttribute("message","you're too young");
+				return "sorry";
+				}
+		
+		else
+		{
+			model.addAttribute("name",name);
+			model.addAttribute("year",year);
+			return "career-selection";
+			
+		}	
+	}
+	
+	@RequestMapping("/career/{option}")
+	public String careerDisplay(@PathVariable String option,Model model)
+	{
+		System.out.println(option);
+		model.addAttribute("option",option);
+		return "career-display";
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/career-selection.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<a id ="sd" href="/career/Software Developer">Software Developer</a><br />
+<a id="sm" href="/career/Scrum Master">Scrum Master</a><br />
+<a id="qa" href="/career/QA Analyst">QA Analyst</a><br />
+<a id="ba" href="/career/Business Analyst">Business Analyst</a><br />
+</body>
+</html>
